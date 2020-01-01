5e50a57b8f73b36932c39d4f8da5cc9d391a5705
ash1969 <nikhil.sonksbs@gmail.com>
2020-01-02 01:06:08 +0530

    Moving The Player

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private bool walkingRight = true;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Switch();
+        }
+    }
+
+    private void Switch() 
+    {
+        walkingRight = !walkingRight;
+
+        if(walkingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, -45, 0);
+        }
+    }
+}
